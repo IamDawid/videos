19d83e5846b96b68d099e4b44cc43c689647b88b
Dawid <david.d.bialy@gmail.com>
2020-05-08 11:43:10 +0100

    some VideoDetail component layout and styling tweaks

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 const VideoDetail = ({ video }) => {
 
+	//if value is null show loading instead to avoid error
 	if (!video) {
 		return <div>Loading...</div>;
 	}
 
-	return <div>{video.snippet.title}</div>
+	return (
+		<div className='ui segment'>
+			<h4 className='ui header'>{video.snippet.title}</h4>
+			<p>{video.snippet.description}</p>
+		</div>
+	);
 }
 
 export default VideoDetail;
