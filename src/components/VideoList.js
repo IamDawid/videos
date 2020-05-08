d2a5f2241cb10037f9be7360fb409de4c4a8f9ac
Dawid <david.d.bialy@gmail.com>
2020-05-08 11:28:14 +0100

    VideoItem to App communication added using deeply nested callback (selectedVideo being passed as props 2 levels down with a callback to App)

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
     //props.videos - videos from this.state.videos in App.js being passed as props 
 
-   const renderedList = videos.map(video => {
-       return <VideoItem video={video} />;
+    const renderedList = videos.map(video => {
+        return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
     });
 
     return <div className='ui relaxed divided list'>{renderedList}</div>;
