1fd456f1564d074e30d31d50ecf769fe678e27e7
Dawid <david.d.bialy@gmail.com>
2020-05-08 11:37:52 +0100

    VideoDetail component added. selectedVideo state is being updated by a deeply nested callback from VideoItem.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 
@@ -19,7 +20,7 @@ class App extends React.Component {
     };
 
     onVideoSelect = (video) => {
-        console.log('From the App', video);
+        this.setState({ selectedVideo: video });
     }
 
     render() {
@@ -27,6 +28,7 @@ class App extends React.Component {
 
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
 
